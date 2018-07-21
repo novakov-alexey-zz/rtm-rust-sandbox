f9b94109d203c7868fddbfe4062959b6e12b7151
Alexey Novakov <an@datawerks.com>
2018-07-21 09:55:39 +0200

    extract constant

--- src/routes/html.rs
+++ src/routes/html.rs
@@ -1,7 +1,7 @@
-use routes::rocket_contrib::Template;
-use core::service::TaskService;
 use core::models::Task;
+use core::service::TaskService;
 use rocket::State;
+use routes::rocket_contrib::Template;
 
 #[derive(Serialize)]
 struct TemplateContext {
@@ -11,9 +11,13 @@ struct TemplateContext {
 
 #[get("/tasks/incomplete")]
 fn all_incompleted_html(service: State<TaskService>) -> Template {
-    let res = service.get_tasks(None, false, None).unwrap();
+    const NAME: &str = "All incompleted tasks";
+    let res = service
+        .get_tasks(None, false, None)
+        .map_err(|e| println!("get tasks error {}", e))
+        .unwrap();
     let context = TemplateContext {
-        name: "All incompleted tasks".to_string(),
+        name: NAME.to_string(),
         items: res,
     };
 
