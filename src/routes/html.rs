8eff54e33784d30a419c188843fc5fc7717bbfe5
Alexey Novakov <an@datawerks.com>
2018-07-22 15:00:55 +0200

    print error in the template

--- src/routes/html.rs
+++ src/routes/html.rs
@@ -7,19 +7,24 @@ use routes::rocket_contrib::Template;
 struct TemplateContext {
     name: String,
     items: Vec<Task>,
+    error: Option<String>,
 }
 
 #[get("/tasks/incomplete")]
 fn all_incompleted_html(service: State<TaskService>) -> Template {
     const NAME: &str = "All incompleted tasks";
-    let res = service
+    let tasks = service
         .get_tasks(None, false, None)
-        .map_err(|e| println!("get tasks error {}", e))
-        .unwrap();
-    let context = TemplateContext {
-        name: NAME.to_string(),
-        items: res,
+        .map_err(|e| format!("get tasks error {}", e));
+    let (items, error) = match tasks {
+        Ok(t) => (t, None),
+        Err(e) => (vec![], Some(e)),
     };
 
+    let context = TemplateContext {
+        name: NAME.to_string(),
+        items,
+        error,
+    };
     Template::render("index", &context)
 }
